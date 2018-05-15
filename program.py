7f6425ab755262ba1f721a0eab2bce83dc58f434
pinkjacket <33609459+pinkjacket@users.noreply.github.com>
2018-05-14 23:23:10 -0700

    creating and listing entries

--- program.py
+++ program.py
@@ -12,27 +12,32 @@ def print_header():
 def run_event_loop():
     print("Welcome to the journal, what do you want to do?")
     cmd = None
+    journal = []
 
     while cmd != "x":
         cmd = input("[L]ist entries, [A]dd a new entry, e[X]it: ")
         cmd = cmd.lower().strip()
 
         if cmd == "l":
-            list_entries()
+            list_entries(journal)
         elif cmd == "a":
-            add_entry()
+            add_entry(journal)
         elif cmd != "x":
             print("Sorry, not an option.")
 
     print("Done, goodbye!")
 
 
-def list_entries():
-    print("Listing...")
+def list_entries(data):
+    print("Journal Entries: ")
+    entries = reversed(data)
+    for idx, entry in enumerate(entries):
+        print("* [{}] {}".format(idx+1, entry))
 
 
-def add_entry():
-    print("Adding...")
+def add_entry(data):
+    text = input("Type your entry, and hit Enter when finished: ")
+    data.append(text)
 
 
 main()
